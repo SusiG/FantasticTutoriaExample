9c2f56760adbc5756185839ba7d93adc597dcf15
SusiG <bhagyalakshmi.guntamadugu@gmail.com>
2018-09-19 12:22:39 +0530

    Added colour to view

--- FantasticTutorial/ViewController.swift
+++ FantasticTutorial/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    
+    var colorCounter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.changeColors()
+        print("colors")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func changeColors(){
+
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in  //1
+            UIView.animate(withDuration: 2.0) {  //2
+                self.view.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor  //3
+                self.colorCounter+=1  //4
+            }
+        }
+        
+        scheduledColorChanged.fire()
+    }
 
 }
 
